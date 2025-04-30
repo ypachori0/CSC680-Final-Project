45aea5961ad0e23c80488163774c24ec5909f919
tpbvirus <relempagotb@gmail.com>
2025-04-29 19:45:39 -0700

    Updated googleservice file with correct data.

--- 680FinalProject/680FinalProject/_80FinalProjectApp.swift
+++ 680FinalProject/680FinalProject/_80FinalProjectApp.swift
@@ -13,14 +13,18 @@ import FirebaseAuth
 import FirebaseFirestore
 import FirebaseStorage
 
+//initializes firestore db
+let db = Firestore.firestore()
+
 //on app launc init firebase
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-
+      
     return true
   }
+
 }
 
 @main
@@ -33,3 +37,5 @@ struct _80FinalProjectApp: App {
         }
     }
 }
+
+
