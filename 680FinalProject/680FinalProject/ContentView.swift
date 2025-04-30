dddeb7fd7dd81c1b48e85b837740c3eb8aeadcac
tpbvirus <relempagotb@gmail.com>
2025-04-30 15:22:02 -0700

    Added code to test reads from db. Need to add way to display data

--- 680FinalProject/680FinalProject/ContentView.swift
+++ 680FinalProject/680FinalProject/ContentView.swift
@@ -6,19 +6,48 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            let backendTest = testBackend()
+
         }
         .padding()
+        
+    }
+}
+
+class testBackend{
+    //function to test pulling data from db.
+    func testData() async -> String {
+        let db = Firestore.firestore()
+        let docRef: DocumentReference = db.collection("events").document("TestEvent")
+        var eventData = ""
+        do {
+          let document = try await docRef.getDocument()
+          if document.exists {
+            let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+            eventData = dataDescription
+            print("Document data: \(dataDescription)")
+          } else {
+            print("Document does not exist")
+          }
+        } catch {
+          print("Error getting document: \(error)")
+        }
+        return eventData
     }
 }
 
 #Preview {
+    
     ContentView()
 }
